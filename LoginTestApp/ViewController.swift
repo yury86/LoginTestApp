26cd92775a4e9493be866e89a87606a48fc0107a
pizza <pizza@pizzas-iMac.local>
2021-10-08 17:27:29 +0900

    Initial Commit

---
+++ LoginTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginTestApp
+//
+//  Created by pizza on 08/10/2021.
+//  Copyright © 2021 pizza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
