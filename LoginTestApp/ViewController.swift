02e8467e3364b467d6437f58605222ccfebf79d1
yury86 <yury86@mail.ru>
2021-10-11 10:22:14 +0900

    commit

--- LoginTestApp/ViewController.swift
+++ LoginTestApp/ViewController.swift
@@ -9,37 +9,14 @@
 import UIKit
 import KeychainAccess
 
-class KeychainSingletone : UIViewController {
-    static
-    @IBAction func save(_ sender: UIButton) {
+class KeychainSingletone {
+    static let sharedInstance = KeychainSingletone()
+    func saveKey(textNameK : String, textPassK : String ) {
         let myKeychain: Keychain
         myKeychain = Keychain()
-        myKeychain[textName.text!] = textPass.text
-        
+        myKeychain[textNameK] = textPassK
     }
-     /*
-     func saveAction(sender: UIButton) {
-     let myKeychain: Keychain
-     keychain = Keychain()
-     keychain[textName.text!] = textPass.text
-     }
-     */
-   
-    @IBAction func editingChanged(_ sender: UITextField) {
-        switch (textName.text, textPass.text) {
-        case let (username?, password?):
-            buttonVhod.isEnabled = !username.isEmpty && !password.isEmpty
-        case (_?, nil):
-            buttonVhod.isEnabled = false
-        case (nil, _?):
-            buttonVhod.isEnabled = false
-        case (nil, nil):
-            buttonVhod.isEnabled = false
-            
-    }
-     private init() {
-        }
-    }
+    private init() {}
 }
 
 class ViewController: UIViewController {
@@ -50,6 +27,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var textPass: UITextField!
    
     
+    @IBAction func save(_ sender: UIButton) {
+        KeychainSingletone.sharedInstance.saveKey(textNameK: textName.text!, textPassK: textPass.text!)
+    }
+    /*
+     func saveAction(sender: UIButton) {
+     let myKeychain: Keychain
+     keychain = Keychain()
+     keychain[textName.text!] = textPass.text
+     }
+     */
+    
+    @IBAction func editingChanged(_ sender: UITextField) {
+        ///let ViewController = super.presentingViewController
+        
+        switch (textName.text, textPass.text) {
+        case let (username?, password?):
+            buttonVhod.isEnabled = !username.isEmpty && !password.isEmpty
+        case (_?, nil):
+            buttonVhod.isEnabled = false
+        case (nil, _?):
+            buttonVhod.isEnabled = false
+        case (nil, nil):
+            buttonVhod.isEnabled = false
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
